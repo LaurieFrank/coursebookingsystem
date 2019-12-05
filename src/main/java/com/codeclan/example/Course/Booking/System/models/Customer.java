9c9b81f81bb38ff2fd765c83bdba1627d0663055
Laurie Frank <laurie360@hotmail.co.uk>
2019-12-05 11:20:05 +0000

    initial commit

---
+++ src/main/java/com/codeclan/example/Course/Booking/System/models/Customer.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.Course.Booking.System.models;
+
+public class Customer {
+}
---
+++ src/test/java/com/codeclan/example/Course/Booking/System/CourseBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.Course.Booking.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
